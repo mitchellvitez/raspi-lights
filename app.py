081d2900895c810fce424e28df47f67d98b85e3c
Mitchell Vitez <mitchell@vitez.me>
2018-12-15 16:35:37 -0800

    Add web app for controlling light patterns

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import os
+import subprocess
+import signal
+
+app = Flask(__name__)
+
+# procedure_name: description
+# procedure_name must match the python filename
+procedures = {
+    'conway': "Conway's Game of Life",
+    'splash': "Raindrops",
+    'bubble_sort': "Sort by Color"
+}
+
+proc = None
+
+@app.route("/")
+def index():
+    global procedures
+    return render_template('index.html', procedures=procedures)
+
+@app.route('/run/<procedure>')
+def run(procedure):
+    global proc
+
+    if proc:
+        pid = proc.pid
+        os.kill(pid, signal.SIGINT)
+
+    proc = subprocess.Popen([f'python3 examples/{procedure}.py'], shell=True)
+
+    print (f"running procedure {procedure}")
+    return (f"running procedure {procedure}")
+
+app.run(host='localhost')
